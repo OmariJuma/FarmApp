a92f7d7e6e442f1db74bdae29d62c48f37b67076
Omar Juma <jumao2669@gmail.com>
2023-06-11 13:08:31 +0300

    working on the routing btwn screens

--- App.js
+++ App.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import {PaperProvider, Drawer, Text} from 'react-native-paper';
-import {ScrollView, StyleSheet, View} from 'react-native';
+import { PaperProvider, Drawer, Text } from 'react-native-paper';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AppBar from './Components/UI/AppBar';
-import CarouselCards from './Components/UI/CarouselCards';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import Chart from './Components/UI/Chart';
-import TabbedLayout from './Components/UI/TabbedLayout';
+import Home from './Components/Pages/Home';
+import Second from './Components/Pages/Second';
+
 function App() {
-  return (
-    <ScrollView>
+  const Stack = createNativeStackNavigator();
+    return (
+    <NavigationContainer>
       <PaperProvider>
-        <SafeAreaView>
-          <AppBar />
-          <View style={styles.container}>
-            <CarouselCards />
-          </View>
-          <TabbedLayout/>
+        <SafeAreaView style={styles.container}>
+          <ScrollView>
+            <AppBar />
+            <Home/>
+            <Stack.Navigator>
+              <Stack.Screen name="Second" component={Second} />
+            </Stack.Navigator>
+          </ScrollView>
         </SafeAreaView>
-        {/* <Chart /> */}
       </PaperProvider>
-    </ScrollView>
+    </NavigationContainer>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignContent: 'center',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    // justifyContent: 'center',
+    justifyContent: 'center',
   },
 });
 
